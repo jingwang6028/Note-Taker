f3eb598c3a007f2cf53c1820ee66544835a295e7
Jing <jingwang6028@gmail.com>
2021-10-14 23:24:31 -0400

    update router folder

---
+++ routers/apiRoute.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const {
+  readFromFile,
+  writeToFile,
+  readAndAppend,
+} = require("../helpers/fsUtils");
+// npm package - shortid
+const shortid = require("shortid");
+
+// GET router
+router.get("/api/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => {
+    res.json(JSON.parse(data));
+  });
+});
+
+// Post router
+router.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  // if title and text is true
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: shortid.generate(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting new note");
+  }
+});
+
+module.exports = router;
---
+++ routers/htmlRoute.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const path = require("path");
+
+// GET /notes should return the notes.html
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// GET * should return the index.html
+
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
